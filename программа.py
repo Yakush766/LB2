154325c1f552ad45e94274f8144e36e13d237491
Yakush766 <anton.yakushenko72@gmail.com>
2024-12-26 19:23:44 +0300

    Внёс изменения в программе Питон

--- программа.py
+++ программа.py
@@ -4,7 +4,7 @@ number = random.randint(1, 100)  # Загаданное число
 print("Угадайте число от 1 до 100!")
 while True:
 guess = int(input("Ваш вариант: "))
-if guess = number:
+if guess == number:
 print("Поздравляем, вы угадали!")
 break
 print("Меньше" if guess > number else "Больше")
